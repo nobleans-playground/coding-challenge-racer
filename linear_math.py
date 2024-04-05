8a3d54ac376148974c92e6af2febab749409ecbc
Ramon Wijnands <ramon.wijnands@nobleo.nl>
2024-04-05 15:34:34 +0200

    Work on linear math lib

--- linear_math.py
+++ linear_math.py
@@ -30,11 +30,19 @@ class Vector:
         return f"Vector({self.x}, {self.y})"
 
 
-class Matrix2x2:
+class Rotation:
     """2x2 rotation matrix"""
 
     def __init__(self, angle):
         self.rows = ((cos(angle), -sin(angle)), (sin(angle), cos(angle)))
+
+    def __getitem__(self, item):
+        if isinstance(item, tuple):
+            return self.rows[item[0]][item[1]]
+        # elif isinstance(item, int):
+        #     return self.rows[item]
+        else:
+            raise IndexError('Invalid index type')
 
     @property
     def angle(self):
@@ -52,35 +60,42 @@ class Matrix2x2:
 class Transform:
     """Rigid 2d transforms with only translation and rotation"""
 
-    def __init__(self, basis: Matrix2x2, origin: Vector):
-        self.basis = basis
-        self.origin = origin
+    def __init__(self, M: Rotation, p: Vector):
+        self.M = M
+        self.p = p
 
 
 def test_vector_add():
     a = Vector(1, 2)
     b = Vector(3, 4)
     c = a + b
+    assert c.x == 4
+    assert c.y == 6
     assert c == Vector(4, 6)
     assert c != Vector(9, 9)
 
 
 def test_matrix2x2_angle():
-    m = Matrix2x2(0)
+    m = Rotation(0)
+    assert m[0, 0] == 1
+    assert m[0, 1] == 0
+    assert m[1, 0] == 0
+    assert m[1, 1] == 1
     assert m.angle == 0
-    m = Matrix2x2(1)
+
+    m = Rotation(1)
     assert m.angle == 1
 
 
 def test_matrix2x2_rotate():
-    m = Matrix2x2(0)
+    m = Rotation(0)
     a = Vector(1, 2)
     b = m * a
     assert b == Vector(1, 2)
 
 
 def test_matrix2x2_rotate_90():
-    m = Matrix2x2(math.pi / 2)
+    m = Rotation(math.pi / 2)
     a = Vector(1, 2)
     b = m * a
     assert b[0] == pytest.approx(-2)
