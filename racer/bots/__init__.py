3708600f03afae83efea46db2de239fb79619cf4
Hein Wessels <heinwessels93@gmail.com>
2024-09-18 18:11:31 +0200

    Add names to botlist

--- racer/bots/__init__.py
+++ racer/bots/__init__.py
@@ -1,7 +1,15 @@
 from .example import SimpleBot
 from .keyboard_bot import KeyboardBot
+from .bram import BrumBot
+from .ivo import SimpleBot as IvoBot
+from .jono import Lightyear
+from .richard import SimpleBot as LilRacer
 
 all_bots = [
     KeyboardBot,
     SimpleBot,
+    BrumBot,
+    IvoBot,
+    Lightyear,
+    LilRacer
 ]
